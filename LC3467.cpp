47fef33a4be4ff3f64a5fbd19c96279d7c5fa55c
redox <a.anandfns@gmail.com>
2025-11-24 23:56:37 +0530

    Transform Array by Parity

--- LC3467.cpp
+++ LC3467.cpp
@@ -5,7 +5,7 @@ public:
             if(nums[i]&1) nums[i] = 1;
             else nums[i] = 0; 
         }
-        sort(nums.begin(), nums.end());
+        sort(nums.begin(),nums.end());
         return nums;
     }
 };
