41180f052e94c5a5c9629bf2ee6b228578fa6689
redox <a.anandfns@gmail.com>
2025-12-15 22:44:07 +0530

    Binary Tree Postorder Traversal

---
+++ LC145.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    void traverse(TreeNode* root, vector<int>& ans) {
+        if(root) {
+            traverse(root->left, ans);
+            traverse(root->right, ans);
+            ans.push_back(root->val);
+        }
+    }
+    vector<int> postorderTraversal(TreeNode* root) {
+        vector<int> ans;
+        traverse(root, ans);
+        return ans;
+    }
+};
