c3e534ed295b76721b158254f5a16a25f28c6bbd
redox <a.anandfns@gmail.com>
2025-12-17 01:42:08 +0530

    Binary Tree Level Order Traversal

---
+++ LC102.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<vector<int>> levelOrder(TreeNode* root) {
+        vector<vector<int>> ans;
+        if(root == nullptr) return ans;
+
+        queue<TreeNode*> q;
+        q.push(root);
+
+        while(!q.empty()) {
+            vector<int> level;
+            int s = q.size();
+            for(int i=0; i<s; i++) {
+                TreeNode* node = q.front();
+                q.pop();
+                if(node->left != nullptr) q.push(node->left);
+                if(node->right != nullptr) q.push(node->right);
+                level.push_back(node->val);
+            }
+            ans.push_back(level);
+        }
+        return ans;
+    }
+};
