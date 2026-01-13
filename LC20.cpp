eb07e6fb0f37a3e87251b32f8918f727f97515d5
redox <a.anandfns@gmail.com>
2026-01-13 15:33:18 +0530

    Valid Parentheses

--- LC20.cpp
+++ LC20.cpp
@@ -25,3 +25,24 @@ public:
         else return false;
     }
 };
+
+// Using Hash
+class Solution {
+public:
+    bool isValid(string s) {
+        map<char, char> hash = {
+            {'(', ')'},
+            {'{', '}'},
+            {'[', ']'}
+        };
+
+        stack<char> st;
+        for(int i=0; i<s.size(); i++) {
+            if(hash.find(s[i]) != hash.end()) st.push(s[i]);
+            else if(!st.empty() && hash[st.top()] == s[i]) st.pop();
+            else st.push(s[i]);
+        }
+
+        return st.empty();
+    }
+};
