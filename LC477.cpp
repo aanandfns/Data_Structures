30cd3fde4624b6529af6d130ca87e5eab3e6b1e2
redox <a.anandfns@gmail.com>
2026-05-07 21:40:18 +0530

    Total Hamming Distance

---
+++ LC477.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int totalHammingDistance(vector<int>& nums) {
+        int n = nums.size();
+        int ans = 0;
+        for(int i=0; i<n; i++) {
+            for(int j=i+1; j<n; j++) {
+                int val = nums[i] ^ nums[j];
+
+                ans += __builtin_popcount(val);
+            }
+        }
+        return ans;
+    }
+};
