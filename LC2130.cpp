e4a7ad6e0f20dec50c32dc101bc5050d4511d038
redox <a.anandfns@gmail.com>
2026-06-14 17:08:55 +0530

    Maximum Twin Sum of a Linked List

---
+++ LC2130.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int pairSum(ListNode* head) {
+        stack<int> s;
+
+        ListNode* mover = head;
+        bool ok = 0;
+        int ans = INT_MIN;
+        int n = 0;
+        while(mover) {n++; mover = mover->next;}
+
+        
+    }
+};
