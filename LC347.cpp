66c62fa2d1a065f9a4e85b535d79f4985dc21280
redox <a.anandfns@gmail.com>
2026-08-12 21:28:57 +0530

    Top K Frequent Elements

---
+++ LC347.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        int n = nums.size();
+
+        map<int, int> mp;
+        for(int i=0; i<n; i++) mp[nums[i]]++;
+
+        vector<pair<int, int>> v;
+        for(auto it : mp) v.push_back({it.second, it.first});
+
+        sort(v.rbegin(), v.rend());
+        
+        vector<int> ans;
+        for(int i=0; i<k; i++) ans.push_back(v[i].second);
+        return ans;
+    }
+};
